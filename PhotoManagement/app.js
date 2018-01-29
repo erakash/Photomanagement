2ed75d2c71f3e77e38b3c944eb02f95b3d310b42
Akash Sharma <akashsharma@Akashs-MacBook-Pro.local>
2018-01-28 23:56:29 -0600

    commit

---
+++ PhotoManagement/app.js
@@ -0,0 +1,33 @@
+﻿var express = require("express");
+var app = express();
+var path = __dirname + '/views/'
+var dbhelper = require('./dbconnection.js');
+app.set('views', __dirname + '/views');
+
+//Aakanksha - Loading the pug view engine
+app.set('view engine', 'pug');
+
+
+app.get("/", function (req, res) {
+    res.sendFile(path + "index.html");
+});
+
+app.get("/contact", function (req, res) {
+    res.sendFile(path + "contact.html");
+});
+
+app.get("/about", function (req, res) {
+    res.sendFile(path + "about.html");
+});
+
+app.get("/search", function (req, res) {
+    res.sendFile(path + "search1.html");
+});
+
+app.listen(3000, function () {
+    console.log("Live at Port 3000");
+});
+
+app.get("/test", function (req, res) {
+    res.render("test", { title: 'Hey', message: 'Hello there!' })
+  })
---
+++ PhotoManagement/dbconnection.js
@@ -0,0 +1,16 @@
+﻿var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    port: 8889,
+    database:"photo_management"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM image_dim", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    });
+  });
